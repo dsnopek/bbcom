6cbed22cb809a4b07125f0316a2fd968110b91e6
David Snopek <dsnopek@gmail.com>
2010-11-30 20:03:44 -0600

    Another IE fix (remove the iframe border)

--- bbcom_embed/bibliobird-view-remote-iframe.tpl.php
+++ bbcom_embed/bibliobird-view-remote-iframe.tpl.php
@@ -1,1 +1,1 @@
-<iframe id="remote-frame" name="remote-frame" border="0" width="100%" height="100%" style="border: 0; width: 100%; height: 100%;" src="<?php print $url ?>"></iframe>
+<iframe id="remote-frame" name="remote-frame" frameBorder="0" width="100%" height="100%" style="border: 0; width: 100%; height: 100%;" src="<?php print $url ?>"></iframe>
