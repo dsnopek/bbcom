f022cb04e54d3b1a68aae6d6ffc691a1aa48903c
David Snopek <dsnopek@gmail.com>
2010-12-01 07:42:13 -0600

    Fixed embed-reader.js build.

--- bbcom_embed/build-embed.sh
+++ bbcom_embed/build-embed.sh
@@ -18,7 +18,7 @@ for profile in *.build.js; do
 	echo "(function () {"      > $name.temp.js
 	cat $STUBS                >> $name.temp.js
 	cat $name.uncompressed.js >> $name.temp.js
-	echo "require(['lingwo_dictionary/annotation/Embed'], function (bb) { bb.start(); });" \
+	echo "require(['bibliobird/Embed'], function (bb) { bb.start(); });" \
 	                          >> $name.temp.js
 	echo "})();"              >> $name.temp.js
 	mv $name.temp.js $name.uncompressed.js
