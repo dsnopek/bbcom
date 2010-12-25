f17311c72bc4ab52e3bc7bae29d9f13c92cbede8
David Snopek <dsnopek@gmail.com>
2010-12-25 09:53:36 -0600

    Fixed problems with translation monkey patch.

--- bbcom_flashcards/bibliobird.py
+++ bbcom_flashcards/bibliobird.py
@@ -14,7 +14,6 @@
 #
 
 import anki.sync
-import anki.lang
 
 # production
 anki.sync.SYNC_URL = 'http://anki.bibliobird.com/sync/'
@@ -28,12 +27,20 @@ anki.sync.SYNC_PORT = 80
 
 # Monkey patch the "_" function to replace the "Create free account" links.
 # TODO: There are probably better ways to do this but this was the easiest!
-create_free_account_link = '<a href="http://www.bibliobird.com/user/register">'
-def t(s):
-    global create_free_account_link
-    return anki.lang._(s).replace('<a href="http://anki.ichi2.net/">', create_free_account_link)
-import ankiqt.forms.preferences
-import ankiqt.ui.main
-ankiqt.forms.preferences._ = t
-ankiqt.ui.main._ = t
+def monkeyPatchTranslations():
+    # get the translation function (is actually ankiqt.ui.main.AnkiQt.getTranslation())
+    import __builtin__
+    orig_t = __builtin__.__dict__['_']
+
+    # our replacement
+    create_free_account_link = '<a href="http://www.bibliobird.com/user/register">'
+    def t(s):
+        return orig_t(s).replace('<a href="http://anki.ichi2.net/">', create_free_account_link)
+
+    # only replace where its needed!
+    import ankiqt.ui.main
+    import ankiqt.forms.preferences
+    ankiqt.ui.main._ = t
+    ankiqt.forms.preferences._ = t
+monkeyPatchTranslations()
 
