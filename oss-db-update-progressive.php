36dd06f6f7bf004f1eefea242afc4d114f96f327
David Snopek <dsnopek@gmail.com>
2011-03-12 13:59:15 -0600

    * Fixed view 'korpus_text_entries'
     * Fixed the entry_hash generation in oss-db-update-progressive.php

--- oss-db-update-progressive.php
+++ oss-db-update-progressive.php
@@ -4,9 +4,10 @@ function populate_lingwo_entry_info() {
   $cur = variable_get('oss_db_update', 0);
   $ret = db_query("SELECT n.nid, le.pos FROM {node} n, {lingwo_entry} le WHERE n.type = '%s' AND le.nid = n.tnid AND n.nid > %d ORDER BY n.nid LIMIT 50", LingwoEntry::$settings->content_type, $cur); 
   while($obj = db_fetch_object($ret)) {
-    $entry = LingwoEntry::fromNid($obj->nid);
-    $node = $entry->getNode();
-    $entry_hash = _lingwo_entry_generate_hash($entry->getNode());
+    $node = node_load($obj->nid);
+    $node->pos = $obj->pos;
+    $entry = LingwoEntry::fromNode($node);
+    $entry_hash = _lingwo_entry_generate_hash($entry);
     $info = (object)array(
       'nid'             => $entry->nid,
       'language'        => implode('-', $entry->getLanguages()),
