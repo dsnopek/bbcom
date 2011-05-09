674b71591fd6d9b04688e9b85de4314d026c3f29
David Snopek <dsnopek@gmail.com>
2011-05-09 10:44:08 -0500

    Began working on custom, experimental voting widget.  It isn't clear whether I'll use it in practice or not.

---
+++ drupal/sites/all/modules/bbcom/bbcom_rate/bbcom-rate-widget.tpl.php
@@ -0,0 +1,39 @@
+<?php
+// $Id: rate-widget.tpl.php,v 1.6 2010/11/12 17:20:47 mauritsl Exp $
+
+/**
+ * @file
+ * Rate widget theme
+ *
+ * This is the default template for rate widgets. See section 3 of the README
+ * file for information on theming widgets.
+ */
+
+$rating = $results['rating'];
+$class = 'neutral';
+if ($rating > 0) {
+  $rating = '+'. $rating;
+  $class = 'positive';
+}
+elseif ($rating < 0) {
+  $class = 'negative';
+}?>
+
+<div class="bbcom-entry-rating bbcom-entry-rating-<?php print $class ?>">
+<div class="bbcom-entry-rating-count"><?php print $rating; ?></div>
+<!-- <div class="bbcom-entry-rating-label"><?php print t('Rating') ?></div> -->
+</div>
+
+<ul class="bbcom-entry-rating-list">
+<?php
+foreach ($links as $link) {
+  if (!empty($results['user_vote']) && $link['value'] == $results['user_vote']) {
+    print '<li><strong>'. $link['text'] .'</strong></li>';
+  }
+  else {
+    print '<li>'. l($link['text'], $link['href']) .'</li>';
+  }
+}
+?>
+</ul>
+
