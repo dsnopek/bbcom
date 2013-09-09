d4d39a7a953d26828cfc46e45b4ebebe3829c017
David Snopek <dsnopek@gmail.com>
2013-09-09 20:05:39 +0100

    * Rewrote more of bbcom_flashcards.anki.inc to work with Anki 2.0.
     * Got the review setup page actually working with Anki 2.0!
    
     * Create the 'bibliobird' deck when setting up a new deck.

--- AnkiServer/BibliobirdAnkiServer/rest_app.py
+++ AnkiServer/BibliobirdAnkiServer/rest_app.py
@@ -3,30 +3,40 @@ import os
 
 from AnkiServer.apps.rest_app import RestHandlerBase, RestApp
 
-def setup_new_collection(col):
-    """Create the 'External' model used by Bibliobird.com."""
-    mm = col.models
+class CollectionInitializer(object):
+    def _setup_model(self, col):
+        """Create the 'External' model used by Bibliobird.com."""
+        mm = col.models
 
-    # NOTE: Supposedly, Anki will check the first field on any model
-    # for uniqueness. Since it's really important to us that the 'External ID'
-    # is unique, we're giving that one first!
+        # NOTE: Supposedly, Anki will check the first field on any model
+        # for uniqueness. Since it's really important to us that the 'External ID'
+        # is unique, we're giving that one first!
 
-    m = mm.new('External')
-    for field_name in ['External ID', 'Front', 'Back']:
-        fm = mm.newField(field_name)
-        mm.addField(m, fm)
+        m = mm.new('External')
+        for field_name in ['External ID', 'Front', 'Back']:
+            fm = mm.newField(field_name)
+            mm.addField(m, fm)
 
-    t = mm.newTemplate('Forward')
-    t['qfmt'] = '{{Front}}'
-    t['afmt'] = '{{FrontSide}}\n\n<hr id=answer>\n\n{{Back}}'
-    mm.addTemplate(m, t)
+        t = mm.newTemplate('Forward')
+        t['qfmt'] = '{{Front}}'
+        t['afmt'] = '{{FrontSide}}\n\n<hr id=answer>\n\n{{Back}}'
+        mm.addTemplate(m, t)
 
-    t = mm.newTemplate('Reverse')
-    t['qfmt'] = '{{Back}}'
-    t['afmt'] = '{{FrontSide}}\n\n<hr id=answer>\n\n{{Front}}'
-    mm.addTemplate(m, t)
+        t = mm.newTemplate('Reverse')
+        t['qfmt'] = '{{Back}}'
+        t['afmt'] = '{{FrontSide}}\n\n<hr id=answer>\n\n{{Front}}'
+        mm.addTemplate(m, t)
 
-    mm.add(m)
+        mm.add(m)
+    
+    def _setup_deck(self, col):
+        """Create the 'bibliobird' deck so we can keep our cards seperate."""
+        col.decks.id('bibliobird', create=True)
+        col.decks.flush()
+    
+    def __call__(self, col):
+        self._setup_model(col)
+        self._setup_deck(col)
 
 # Our entry point
 def make_app(global_conf, **local_conf):
@@ -37,7 +47,7 @@ def make_app(global_conf, **local_conf):
     app = RestApp(
         data_root=local_conf['data_root'],
         allowed_hosts=local_conf.get('allowed_hosts', '127.0.0.1'),
-        setup_new_collection=setup_new_collection
+        setup_new_collection=CollectionInitializer()
     )
 
     return app
