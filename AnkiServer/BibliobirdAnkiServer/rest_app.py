28f03a1286ad5bb55a0950e87f1c6707ec3b4237
David Snopek <dsnopek@gmail.com>
2013-09-10 16:25:33 +0100

    Got the entire review "flow" working!

--- AnkiServer/BibliobirdAnkiServer/rest_app.py
+++ AnkiServer/BibliobirdAnkiServer/rest_app.py
@@ -4,7 +4,7 @@ import os
 from AnkiServer.apps.rest_app import RestHandlerBase, RestApp
 
 class CollectionInitializer(object):
-    def _setup_model(self, col):
+    def _setup_model(self, col, did):
         """Create the 'External' model used by Bibliobird.com."""
         mm = col.models
 
@@ -13,6 +13,7 @@ class CollectionInitializer(object):
         # is unique, we're giving that one first!
 
         m = mm.new('External')
+        m['did'] = did
         for field_name in ['External ID', 'Front', 'Back']:
             fm = mm.newField(field_name)
             mm.addField(m, fm)
@@ -31,12 +32,13 @@ class CollectionInitializer(object):
     
     def _setup_deck(self, col):
         """Create the 'bibliobird' deck so we can keep our cards seperate."""
-        col.decks.id('bibliobird', create=True)
+        did = col.decks.id('bibliobird', create=True)
         col.decks.flush()
+        return did
     
     def __call__(self, col):
-        self._setup_model(col)
-        self._setup_deck(col)
+        did = self._setup_deck(col)
+        self._setup_model(col, did)
 
 # Our entry point
 def make_app(global_conf, **local_conf):
--- drupal/sites/all/modules/bbcom/bbcom_flashcards/bbcom_flashcards.js
+++ drupal/sites/all/modules/bbcom/bbcom_flashcards/bbcom_flashcards.js
@@ -3,21 +3,22 @@ Drupal.behaviors.bibliobird_flashcards = function (context) {
     var link = $('<a id="bibliobird-flashcards-show-answer" href="#"></a>')
         .text(Drupal.t('Show answer'))
         .click(function (evt) {
-            $('#bibliobird-flashcards-flashcard-answer').css('visibility', 'visible');
+            $('#bibliobird-flashcards-flashcard-question').hide();
+            $('#bibliobird-flashcards-flashcard-answer').show();
             $('#bibliobird-flashcards-ease-buttons').show();
             link.remove();
             evt.preventDefault();
         })
         .insertAfter($('#bibliobird-flashcards-flashcard', context));
 
-    // setup our tabs
-    $('#bibliobird-flashcards-study-options', context).tabs();
+    function updateStudyOptions() {
+      var type = '' + $('#edit-study-options-type').val();
+      $('.bibliobird-flashcards-study-options-toggle').hide();
+      $('.bibliobird-flashcards-study-options-' + type).show();
+    }
 
-    // should use .unwrap() but this is jquery 1.2
-    $('#tab-new-cards fieldset, #tab-reviews fieldset', context).each(function () {
-        var n = $(this);
-        $('legend', n).remove();
-        n.replaceWith(n.contents());
-    });
+    // Hide/show fields depending on current review type
+    $('#edit-study-options-type', context).change(updateStudyOptions);
+    updateStudyOptions();
 };
 
