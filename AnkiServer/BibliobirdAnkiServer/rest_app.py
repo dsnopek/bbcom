3137af9a1f14d930eb5fa9d9314db870984c2aa6
David Snopek <dsnopek@gmail.com>
2013-09-10 16:58:37 +0100

    Reimplemented the "Re-sync user's flashcards" operation.

--- AnkiServer/BibliobirdAnkiServer/rest_app.py
+++ AnkiServer/BibliobirdAnkiServer/rest_app.py
@@ -40,6 +40,45 @@ class CollectionInitializer(object):
         did = self._setup_deck(col)
         self._setup_model(col, did)
 
+class CollectionHandler(RestHandlerBase):
+    """Custom collection funtions for Bibliobird."""
+
+    def resync_notes(self, col, req):
+        # build a fast lookup table
+        lookup = dict([(id, True) for id in req.data.get('external_ids', [])])
+
+        remove = []
+
+        # Loop through all the notes on the 'bibliobird' deck - adding any to the
+        # remove list which aren't on the lookup table, and pruning all the ids
+        # that we've found from the lookup table (leaving only the missing ids)
+        for nid in col.findNotes('deck:"bibliobird"'):
+            note = col.getNote(nid)
+            try:
+                id = note['External ID']
+            except:
+                id = False
+
+            # Remove any notes with no 'External ID'
+            if not id:
+                remove.append(nid)
+                continue
+
+            # Remove any notes which can't be found in the lookup list
+            if not lookup.has_key(id):
+                remove.append(nid)
+            else:
+                del lookup[id]
+
+        # Remove all the notes on the remove list
+        col.remNotes(remove)
+
+        print "remove: ", remove
+        print "missing: ", lookup.keys()
+
+        # Return the missing list
+        return {'missing': lookup.keys()}
+
 # Our entry point
 def make_app(global_conf, **local_conf):
     # setup the logger (copied from AnkiServer.apps.rest_app.make_app())
@@ -51,6 +90,8 @@ def make_app(global_conf, **local_conf):
         allowed_hosts=local_conf.get('allowed_hosts', '127.0.0.1'),
         setup_new_collection=CollectionInitializer()
     )
+
+    app.add_handler_group('collection', CollectionHandler())
 
     return app
 
